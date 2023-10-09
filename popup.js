c93da2b15fcdeda9cf7a617048a97462582ac693
Xiakeming <xiakeming97@gmail.com>
2023-10-09 22:39:19 +0800

    ✨ fix: 更新一些功能

--- popup.js
+++ popup.js
@@ -3,6 +3,11 @@ const textarea = document.getElementById("story");
 const copyButton = document.querySelector(".bj-copy");
 const shearButton = document.querySelector(".bj-shear");
 const emptyButton = document.querySelector(".bj-empty");
+const uppercaseButton = document.getElementById("all-uppercase");
+const lowercaseButton = document.getElementById("all-lowercase");
+const spaceUnderInButton = document.getElementById("space-under-in");
+const underInSpaceTurnHumpButton = document.getElementById("under_in_space_turn_hump");
+
 
 // 复制文本
 copyButton.addEventListener("click", () => {
@@ -24,4 +29,34 @@ shearButton.addEventListener("click", () => {
 emptyButton.addEventListener("click", () => {
   // 清空文本区域的内容
   textarea.value = "";
+});
+
+// 点击按钮将文本转换为全大写
+uppercaseButton.addEventListener("click", () => {
+  const currentText = textarea.value;
+  const uppercaseText = currentText.toUpperCase();
+  textarea.value = uppercaseText;
+});
+
+// 点击按钮将文本转换为全小写
+lowercaseButton.addEventListener("click", () => {
+  const currentText = textarea.value;
+  const uppercaseText = currentText.toLowerCase()
+  textarea.value = uppercaseText;
+});
+
+// 点击按钮将文本空格转换为下划线
+spaceUnderInButton.addEventListener("click", () => {
+  const currentText = textarea.value;
+  const uppercaseText = currentText.replace(' ','_')
+  textarea.value = uppercaseText;
+});
+
+// 点击按钮将文本空格转换为下划线
+underInSpaceTurnHumpButton.addEventListener("click", () => {
+  const currentText = textarea.value;
+  const uppercaseText = currentText.replace(/(_|\s)([a-z])/g, function(match,separator, letter) {
+    return letter.toUpperCase();
+  });
+  textarea.value = uppercaseText;
 });
